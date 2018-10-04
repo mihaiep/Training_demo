ca68965470cf3e6517f401e59bdccff65d88088f
Mihai Ene-Pietrosanu <mihai.enepietrosanu@gmail.com>
2018-10-04 15:25:12 -0700

    Add files via upload

---
+++ Make folder/Name.py
@@ -0,0 +1,6 @@
+name = input ("Your name? ")
+print ("Hi" + name + "How old you are?")
+age = int (input())
+print ("Hi" + name + "So you are" + str (age) + " years old. Can you tell me your height?")
+height = float (input ())
+print ("So " + name + " you are" + str (age) + "years old and you are " + str (height) +"tall")
