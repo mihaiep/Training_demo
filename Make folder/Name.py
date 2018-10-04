1e58cb188055c0c3cea490c4a0500183289af4db
Mihai Ene-Pietrosanu <mihai.enepietrosanu@gmail.com>
2018-10-04 15:26:08 -0700

    Update Name.py

--- Make folder/Name.py
+++ Make folder/Name.py
@@ -4,3 +4,4 @@ age = int (input())
 print ("Hi" + name + "So you are" + str (age) + " years old. Can you tell me your height?")
 height = float (input ())
 print ("So " + name + " you are" + str (age) + "years old and you are " + str (height) +"tall")
+* Comment
