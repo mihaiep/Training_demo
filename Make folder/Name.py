6ec0285052cb3817e8a2d10162b9c1c436518b23
Mihai Ene-Pietrosanu <mihai.enepietrosanu@gmail.com>
2019-04-04 14:00:41 -0700

    Delete Name.py

--- Make folder/Name.py
+++
@@ -1,8 +0,0 @@
-name = input ("Your name? ")
-print ("Hi" + name + "How old you are?")
-age = int (input())
-print ("Hi" + name + "So you are" + str (age) + " years old. Can you tell me your height?")
-height = float (input ())
-print ("So " + name + " you are" + str (age) + "years old and you are " + str (height) +"tall")
-# Comment
-if (name=Hi) 
